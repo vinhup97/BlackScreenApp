9b0cd562f3cb0896a6c31f85442cbca7841bc340
vinhup97 <vinhup97@gmail.com>
2025-06-10 15:26:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/blackscreenapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.blackscreenapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        )
+
+        window.decorView.setBackgroundColor(android.graphics.Color.BLACK)
+    }
+}
